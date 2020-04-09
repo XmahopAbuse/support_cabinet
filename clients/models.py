b149a7d13d75571628b5e418c4fee853e4ad1f44
xmahopabuse <xmahopnyan@yandex.ru>
2020-04-09 09:38:02 +0300

    add clients app

--- clients/models.py
+++ clients/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class NewClient(models.Model):
     STATUS = [
         ('OK', 'Договор заключен'),
@@ -9,11 +10,22 @@ class NewClient(models.Model):
         ('Another', 'Другое'),
     ]
 
+    FROM = [
+        ('Site', 'С сайта'),
+        ('Agent', 'Агент'),
+        ('Office', 'В офисе'),
+        ('Phone','По телефону')
+    ]
+
     city = models.CharField("Город",max_length=100)
     name = models.CharField("Имя", max_length=100)
     phone = models.CharField("Номер телефона", max_length=100)
     message = models.CharField("Сообщение", max_length=100, blank=True)
-    is_send = models.BooleanField("Отправлено в телеграм", default=None, )
-    manager = models.CharField("Менеджер", max_length=100, blank=True)
-    status = models.CharField(max_length=10, choices=STATUS, blank=True)
-    message_id = models.IntegerField("message id", default=None, blank=True)
+    is_send = models.BooleanField("Отправлено в телеграм",null=True, default=None, )
+    manager = models.CharField("Менеджер", max_length=100,null=True, blank=True)
+    status = models.CharField(max_length=10, choices=STATUS,null=True, blank=True)
+    message_id = models.IntegerField("message id", null=True,default=None, blank=True)
+    reply_id = models.IntegerField("reply_id", null=True,default=None, blank=True)
+    comment = models.TextField("Комментарий", max_length=255,null=True, blank=True)
+    ticket_from =  models.CharField("Заявка",max_length=10, choices=FROM,null=True, blank=True)
+    district = models.CharField("Район",max_length=100,null=True, blank=True)
